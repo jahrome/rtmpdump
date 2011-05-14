c5b90b97d356fb456806142b5fe9692f1db0ab8c
Jahrome <jahrome11@gmail.com>
2011-05-14 23:07:48 +0200

    Build both static and shared library

--- librtmp/Android.mk
+++ librtmp/Android.mk
@@ -22,3 +22,26 @@ LOCAL_CFLAGS := \
 
 LOCAL_PRELINK_MODULE := false
 include $(BUILD_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS:=eng debug
+
+LOCAL_SRC_FILES:= \
+	amf.c		\
+	hashswf.c	\
+	log.c		\
+	parseurl.c	\
+	rtmp.c
+
+LOCAL_MODULE:= librtmp
+
+LOCAL_C_INCLUDES := 			\
+	$(LIBRTMP_TOP)			\
+	$(LOCAL_PATH)
+
+LOCAL_CFLAGS := \
+	-DNO_CRYPTO
+
+LOCAL_PRELINK_MODULE := false
+include $(BUILD_SHARED_LIBRARY)
